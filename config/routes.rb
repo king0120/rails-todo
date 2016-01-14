4c66c03566c32f59023832683fbd80fdaa1b2be1
Jamie King <king0120@gmail.com>
2016-01-14 11:48:08 -0500

    Added MVC CRUD for User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :todos
   root to: 'static_pages#home'
   match '/about', to: 'static_pages#about', via: 'get'
+  match 'todos/:id/toggle_completed', to: 'todos#toggle_completed', via: 'get'
 end
